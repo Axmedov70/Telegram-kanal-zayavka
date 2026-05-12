ccad46f081c582afa0c84e6a1fff82d606c24a56
Axmedov <sizning_emailingiz@gmail.com>
2026-05-12 19:02:24 +0500

    final fix for dependencies

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
+import os
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
+from aiohttp import web
+
+
 
 # Tokeningizni saqlab qoldim
 API_TOKEN = '8490179708:AAEFgO2e3eYLfP4cb7H2SeXuVfJiXZEDyBc'
@@ -28,3 +32,19 @@ if __name__ == '__main__':
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
         logging.info("Bot to'xtatildi")
+
+
+
+async def handle(request):
+    return web.Response(text="Bot is running!")
+
+async def on_startup(dispatcher: Dispatcher):
+    app = web.Application()
+    app.router.add_get("/", handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", int(os.getenv("PORT", 8080)))
+    await site.start()
+
+# Dispatcher-ni ishga tushirayotganda on_startup-ni bog'lang
+# Masalan: await dp.start_polling(bot, on_startup=on_startup)
